09bdf8a7fee65e2bef6655093ec8ef7c1a7b55f9
kelknight06 <kelknight06@gmail.com>
2026-08-28 19:56:27 -0500

    Add files via upload

---
+++ MyApplication.java
@@ -0,0 +1,31 @@
+package lab6;
+import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import javafx.scene.control.cell.TextFieldListCell;
+public class MyApplication extends Application {
+
+	public MyApplication() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
